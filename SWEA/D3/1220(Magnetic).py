1a4bc3cb2443315d5334094c6dc7d825d081d6b5
Gunhoo Park <gunhoo2016@gmail.com>
2022-11-01 15:30:12 +0900

    remove Reverse Array

--- SWEA/D3/1220(Magnetic).py
+++ SWEA/D3/1220(Magnetic).py
@@ -8,20 +8,19 @@ for test_case in range(1, 11):
 
     for i in range(0, h):
         arr[i] = list(map(int, input().split()))
-    arr = list(zip(*arr))
 
     # 1 : N / 2 : S
     for i in range(0,h):
         q = []
         for j in range(0, h):
-            if arr[i][j] != 0:
-                q.append(arr[i][j])
+            if arr[j][i] != 0:
+                q.append(arr[j][i])
 
         while len(q)>0:
             if q[0] == 2:
                 del q[0]
             if q[-1] == 1:
-                del q[len(q)-1]
+                del q[-1]
             if q[0] != 2 and q[-1] != 1:
                 break
         for k in range(0, len(q)-1):
