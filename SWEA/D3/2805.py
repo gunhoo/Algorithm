451d5b33862c24f502f0107f3416f2b4f2be0764
Gunhoo Park <gunhoo2016@gmail.com>
2022-11-03 16:29:51 +0900

    solved 2805

---
+++ SWEA/D3/2805.py
@@ -0,0 +1,25 @@
+T = int(input())
+
+for test_case in range(1, T+1):
+    n = int(input())
+    arr = [0]*n
+    sum = ''
+    ans = 0
+
+    for i in range(n):
+        arr[i] = input()
+    for i in range(0, n//2):
+        sum += arr[i][n//2-i:n//2+i+1]
+    for i in range(n//2, n):
+        sum += arr[i][i-n//2:n-i+n//2]
+    for i in sum:
+        ans += int(i)
+    # sum += arr[n//2]
+
+
+    print("#{} {}".format(test_case, ans))
+    # for i in range(len(arr)):
+    #     print(arr[i], end="")
+    #     if (i+1)%10 == 0 :
+    #         print()
+    # print()
