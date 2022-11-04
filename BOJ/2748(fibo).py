6053b9542c008c3ab6f54101a241ee51708769d8
Gunhoo Park <gunhoo2016@gmail.com>
2022-11-04 12:41:27 +0900

    solved 2748(fibonacci with DP)

---
+++ BOJ/2748(fibo).py
@@ -0,0 +1,14 @@
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        fib_arr = [0,1]
+        for i in range(2, n+1):
+            fib_arr.append(fib_arr[i-1]+fib_arr[i-2])
+        return fib_arr[n]
+
+n = int(input())
+
+print(fibo(n))
