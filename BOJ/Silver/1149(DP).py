3d403c28baaab702fbef5d69a9cadb0f14dd0dbe
Gunhoo Park <gunhoo2016@gmail.com>
2022-11-10 14:24:58 +0900

    update 1149(DP) with better memory

--- BOJ/Silver/1149(DP).py
+++ BOJ/Silver/1149(DP).py
@@ -2,15 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-
-dp = [[0]*3 for _ in range(n)]
 graph = []
 for i in range(n):
      graph.append(list(map(int, input().split())))
-dp[0] = graph[0]
 
 for i in range(1, n):
-    for j in range(3):
-        dp[i][j] = min(dp[i-1][j-1], dp[i-1][j-2]) + graph[i][j]
+    graph[i][0] += min(graph[i-1][1], graph[i-1][2])
+    graph[i][1] += min(graph[i - 1][0], graph[i - 1][2])
+    graph[i][2] += min(graph[i - 1][1], graph[i - 1][0])
 
-print(min(dp[n-1]))
+print(min(graph[n-1]))
