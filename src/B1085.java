4afcd6a8a7de15eeaeaf8d1ed6e3dbe6e0506c0a
Gunhoo Park <gunhoo2016@naver.com>
2022-12-28 11:32:03 +0900

    For Eclipse

---
+++ src/B1085.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class B1085 {
+
+	public static void main(String[] args)throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		int w = scanner.nextInt();
+		int h = scanner.nextInt();
+		int arrays[] = {x-0, y-0, w-x, h-y};
+		Arrays.sort(arrays);
+		System.out.println(arrays[0]);
+
+	}
+
+}
