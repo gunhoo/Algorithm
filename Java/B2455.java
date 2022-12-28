6b504fa0e81e6b5bfda75fcdcc0b6e5379ce8f1c
Gunhoo Park <gunhoo2016@gmail.com>
2022-12-28 12:20:21 +0900

    solved B2455

---
+++ Java/B2455.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class B2455 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		int d = scanner.nextInt();
+		int e = scanner.nextInt();
+		int f = scanner.nextInt();
+		int g = scanner.nextInt();
+		int h = scanner.nextInt();
+		int arrays[] = {0-a+b, 0-a+b-c+d, 0-a+b-c+d-e+f, 0-a+b-c+d-e+f-g+h};
+		Arrays.sort(arrays);
+		System.out.println(arrays[3]);
+
+	}
+
+}
