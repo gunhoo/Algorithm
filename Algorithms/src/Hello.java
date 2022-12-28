ce65e008cfd7f877826409cefc579b88ba1e54ab
Gunhoo Park <gunhoo2016@gmail.com>
2022-12-28 11:10:11 +0900

    for Java

---
+++ Algorithms/src/Hello.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ctrl+shft+l	단축기 전체 목록보기
+		// ctrl + - 폰트크기 증가/감소
+		// ctrl D 한줄 삭제
+		// ctrl+alt+down 행단위 복사
+		// alt+shft+A 멀티컬럼 편집
+		// alt+up, dwon 행단위 이동
+		// ctrl+i 자동 들여쓰기
+		// ctrl+space 자동완성 sysout 
+		System.out.println();
+		System.out.println("Helloabc world");
+		System.out.println("Helloabc world");
+		System.out.println("Helloabc world");
+		int a = 1;
+		float f = 3.14f;
+		double d = 3.14f;
+		System.out.println(d);
+		char ch = 'a';
+		String s = "ABC";
+		
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		int num2 = scanner.nextInt();
+		String num3 = scanner.nextLine();
+		
+		System.out.println(num+num2);
+		System.out.println(num3);
+		
+	}
+
+}
