31d9adb311a2ea43e6c81550e6e433f1ee03acd3
Gunhoo Park <gunhoo2016@gmail.com>
2023-02-02 09:27:26 +0900

    Update BJ_15486.java

--- Java/Gold/BJ_15486.java
+++ Java/Gold/BJ_15486.java
@@ -1,5 +1,4 @@
 package Gold;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,9 +14,7 @@ import java.util.StringTokenizer;
  * 
  * 1. 문제 풀이 유형?
  * 		- DP 문제인듯?
- * 
- * 
- * 
+ *  
  * @author Gunhoo
  *
  */
@@ -29,42 +26,36 @@ public class BJ_15486 {
 		int n = Integer.parseInt(br.readLine());
 		int[] t = new int[n+1];
 		int[] p = new int[n+1];
-		boolean[] visited = new boolean[n];
-		
+		// boolean[] visited = new boolean[n];
+		int max = 0;
 		for(int i = 0; i< n ; i++) {
 			st = new StringTokenizer(br.readLine());
 			t[i] = Integer.parseInt(st.nextToken());
 			p[i] = Integer.parseInt(st.nextToken());
 		}
-		/* 최초 시도 : 시간초과 : 원인? 탐색 too much
+		/* 최초 시도 (시간초과) : 원인? 탐색 : O(nlongn)? O(n^2)?
 		for(int i = 0 ; i< n ; i++) {
 			int idx = 0;
-			// arrayList를 만들어서
-			ArrayList<Integer> canMax = new ArrayList<>();
+            
 			if((i+t[i])>n ) { // 범위를 넘어가는 보상은 의미 없으므로 0으로 초기화
 				p[i] = 0;
 			}
-			for(int j = idx; j < i ; j++) {
-				if(visited[j] == false && (j+t[j])<= i) {
-					canMax.add(p[i]+p[j]);
-					visited[j-1>0?j-1:0] = true;
-//					System.out.println("i: "+i+"j: "+j+"visitied: "+visited[j]);
+			for(int j = idx; j < i ; j++) { // 이전 max를 저장한 값부터 i 까지 탐색
+				if(visited[j] == false && (j+t[j])<= i) { // 방문하지 않은 곳이고 j+t[j]가 i를 넘지 않으면
+                    max = Math.max(max, p[i]+p[j]); // max를 i+j와 현재까지의 max값과 비교하여 업데이트
+					visited[j-1>0?j-1:0] = true; // 방문끝났으므로 true 로 변환
 				}
 			}
-			if(canMax.size() > 0) {
-				Collections.sort(canMax, Collections.reverseOrder());
-				p[i] = canMax.get(0);
-				idx = i;
+            if(max > 0){ // max가 초기값이 아니고 갱신된 값이면
+                p[i] = max; // 현재(p[i]) 이익을 현재까지의 max로 넣어준다.
+				idx = i; // idx 를 i로 갱신
 			}
 		}
-
-		Arrays.sort(p);
-		System.out.println(p[p.length-1]);
-		*/
+        System.out.println(max);*/
 		
-		// dp
+		
+		// dp with not recursive
 		int[] dp = new int[n+1];
-		int max = 0;
 		for(int i = 0; i< n ; i++) {
 			max = Math.max(max, dp[i]); // 현재값이랑 기존의 max값 비교하여 max 업데이트
 			if((i+t[i]) <= n){ // 넘어가지 않는 범위면 
@@ -73,7 +64,6 @@ public class BJ_15486 {
 		}
 		max = Math.max(max, dp[n]);
 		System.out.println(max);
-
 	}
 
 }
