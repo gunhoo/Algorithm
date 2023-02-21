3bc245310457e4f1b67d61b99d1241a1ab72f285
gunhoo <gunhoo2016@gmail.com>
2023-02-21 09:51:18 +0900

    solved BJ 17144(G4, implementation/simulation, graph)

--- Java/Gold/BJ_17144_미세먼지안녕.java
+++ Java/Gold/BJ_17144_미세먼지안녕.java
@@ -20,7 +20,7 @@ public class BJ_17144_미세먼지안녕 {
 	static int puriBot; // 공기청정기의 밑부분의 행 값 저장
 	static int[] dx = {-1,0,1,0};
 	static int[] dy = {0,1,0,-1};
-	static int totalDust = 0;
+//	static int totalDust = 0; // 총먼지량 저장
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,42 +36,25 @@ public class BJ_17144_미세먼지안녕 {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				if (map[i][j] == -1) {
 					puriBot = i; // 밑에있는 공기청정기의 행 값 저장
-				}else if(map[i][j] != 0) {// 공기청정기 아니고 빈공간아니면: 미세먼지있으면
-					totalDust += map[i][j];
 				}
+//				else if(map[i][j] != 0) {// 공기청정기 아니고 빈공간아니면: 미세먼지있으면
+//					totalDust += map[i][j];
+//				}
 			}
 		}
 		for(int i = 0; i< t ; i++) { // t초만큼 수행
 			spreadDust();
-//			System.out.println("*****spread*********");
-//			for(int ii = 0; ii < r ; ii++) {
-//				for(int j = 0 ; j< c ; j++) {
-//					System.out.print(map[ii][j]+" ");
-//				}
-//				System.out.println();
-//			}
-//			System.out.println();
+//			System.out.println("*****spread******");
+//			printMap();
 			purifyUpper();
 			purifyBelow();
-//			System.out.println("*****purify*********");
-//			for(int ii = 0; ii < r ; ii++) {
-//				for(int j = 0 ; j< c ; j++) {
-//					System.out.print(map[ii][j]+" ");
-//				}
-//				System.out.println();
-//			}
-//			System.out.println();
+//			System.out.println("*****purify******");
+//			printMap();
 		}
-//		System.out.println();
-//		for(int i = 0; i < r ; i++) {
-//			for(int j = 0 ; j< c ; j++) {
-//				System.out.print(map[i][j]+" ");
-//			}
-//			System.out.println();
-//		}
-		System.out.println(totalDust);
-//		System.out.println(sumDusts());
+//		System.out.println(totalDust);
+		System.out.println(sumDusts());
 	}
+	
 	private static int sumDusts() {
 		int sum = 0;
 		for(int i = 0; i < r ; i++) {
@@ -83,72 +66,38 @@ public class BJ_17144_미세먼지안녕 {
 		}
 		return sum;
 	}
+	
 	private static void purifyUpper() {
-		if(map[puriBot-2][0] != 0 ) { // 공기청정기 위쪽이 미세먼지면
-			totalDust -= map[puriBot-2][0];  // 미세먼지 냠냠
-		}
-		int tmp = map[puriBot-1][c-1]; // 윗부분의 오른쪽아래 부분 
-		for(int i = c-1; i> 1; i--) {
-			map[puriBot-1][i] = map[puriBot-1][i-1]; // 갱신 
-		}
-		map[puriBot-1][1] = 0; // 새로운 바람이 부니까 그건 0임
-		
-		int tmp2 = map[0][c-1]; // 윗부분의 오른쪽 윗부분
-		for(int i =0 ; i <puriBot-1 ; i++) {
-			map[i][c-1] = map[i+1][c-1];
-		}
-		map[puriBot-2][c-1] = tmp;
-		
-		tmp = map[0][0];
-		for(int i = 0 ; i < c-2 ; i++) { // 윗라인 교체
-			map[0][i] = map[0][i+1];
-		}
-		map[0][c-2] = tmp2; 
-		
-		for(int i = 2 ; i< puriBot-1; i++) {
-			map[i][0] = map[i-1][0];
-		}
-		map[1][0] = tmp;
-		
+//		if(map[puriBot-2][0] != 0 ) { // 공기청정기 위쪽이 미세먼지면
+//			totalDust -= map[puriBot-2][0];  // 미세먼지 냠냠
+//		}
+		for (int i = puriBot-2; i >= 1; i--) map[i][0] = map[i-1][0];
+        for (int i = 0; i <= c - 2; i++) map[0][i] = map[0][i+1];
+        for (int i = 0; i <= puriBot-2; i++) map[i][c-1] = map[i+1][c-1];
+        for (int i = c - 1; i >= 2; i--) map[puriBot-1][i] = map[puriBot-1][i-1];
+        map[puriBot-1][1] = 0;
 	}
-	private static void purifyBelow() {
-		if(map[puriBot+1][0] != 0 ) {
-			totalDust -=map[puriBot+1][0]; 
-		}
-		int tmp = map[puriBot][c-1]; // 아래부분의 오른쪽위 부분 
-		for(int i = c-1; i> 1; i--) {
-			map[puriBot][i] = map[puriBot][i-1]; // 갱신 
-		}
-		map[puriBot][1] = 0; // 새로운 바람이 부니까 그건 0임
-		
-		int tmp2 = map[r-1][c-1]; // 아래부분의 오른쪽 아래부분
-		for(int i = r-1 ; i >puriBot ; i--) {
-			map[i][c-1] = map[i-1][c-1];
-		}
-		map[puriBot+1][c-1] = tmp;
-		
-		tmp = map[r-1][0];
-		for(int i = 0 ; i < c-2 ; i++) { // 윗라인교체
-			map[r-1][i] = map[r-1][i+1];
-		}
-		map[r-1][c-2] = tmp2; 
-		
-		for(int i = puriBot+1 ; i < r-2; i++) {
-			map[i][0] = map[i+1][0];
-		}
-		map[r-2][0] = tmp;
-		
-	}
+	private static void purifyBelow(){
+//		if(map[puriBot+1][0] != 0 ) { // 아래쪽 미세먼지 존재하면
+//			totalDust -=map[puriBot+1][0];  // 냠냠
+//		}
+        for (int i = puriBot + 1; i <= r - 2; i++) map[i][0] = map[i+1][0];
+        for (int i = 0; i <= c - 2; i++) map[r-1][i] = map[r-1][i+1];
+        for (int i= r-1; i >= puriBot + 1; i--) map[i][c-1] = map[i-1][c-1];
+        for (int i = c-1; i >= 2; i--) map[puriBot][i] = map[puriBot][i-1];
+        map[puriBot][1] = 0;
+    }
+	
 	
 	private static void spreadDust() {
-		for(int i = 0; i< map.length; i++) {
+		for(int i = 0; i< map.length; i++) { // map 정보 복사
 			tmpMap[i] = Arrays.copyOf(map[i], map[i].length);
 		}
 		for(int i = 0; i< r ; i++) {
 			for(int j = 0 ; j< c; j++) {
-				if( map[i][j] <5 )continue; // 5보다 작으면 퍼지지 않아
+				if( map[i][j] <5 )continue; // 5보다 작으면 퍼지지 않아(볼필요없음)
 				for(int k = 0 ; k< 4 ; k++) { // 상하좌우 봐서
-					int nx = i + dx[k];
+					int nx = i + dx[k]; // 좌표이동
 					int ny = j + dy[k];
 					if( 0<= nx && nx<r && 0<=ny && ny<c && map[nx][ny] != -1) { // 이동한것이 범위내에 있고, 공기청정기 아니면
 						tmpMap[nx][ny] += map[i][j]/5; // 이동한 새로운좌표에 기존의 5분의1만큼 추가하고
