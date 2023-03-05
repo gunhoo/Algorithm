5a22c9fd551e68884517f1623f29a68fa2ec0e56
Gunhoo Park <gunhoo2016@gmail.com>
2023-03-05 23:16:48 +0900

    solved SW 2105 implementation

---
+++ Java/SWTest/SW_2105_디저트카페.java
@@ -0,0 +1,122 @@
+package SWTest;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 맨위에서부터
+ * 	1부터 n-1까지 보면서
+ * 		size = 1~n-1까지 
+ * 		대각선으로 size만큼 가면서 size만큼 다 가거나, 0또는n-1만나면 꺾어 
+ * @author Gunhoo
+ *
+ */
+public class SW_2105_디저트카페 {
+	static int N;
+	static int[][] map;
+	static boolean[] visited;
+	static int answer;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int t = Integer.parseInt(st.nextToken());
+		StringBuilder sb =new StringBuilder();
+		for(int test_case = 1; test_case <= t; test_case++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			map = new int[N][N];
+			answer = -1;
+			for(int i =0 ; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j =0 ; j < N ; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			bfs();
+			sb.append("#").append(test_case).append(" ").append(answer).append("\n");
+		}
+		System.out.print(sb);
+	}
+	static int[] dx = {1,1,-1,-1};
+	static int[] dy = {-1,1,1,-1};
+	
+	private static void bfs() {
+		for(int size=1; size<=N-1; size++) {
+			for(int size2 = 1; size2 <= N-1; size2++) {
+				for(int i =0 ; i <= N-3; i++) {
+					out :for(int j = 1; j < N-1; j++) {
+						visited = new boolean[101];
+						int dir = 0;
+						visited[map[i][j]] = true;
+						int nx = i;
+						int ny = j;
+						for(int k = 0 ; k < size2; k++) {
+							nx = nx + dx[dir];
+							ny = ny + dy[dir];
+							if(visited[map[nx][ny]]) continue out; 
+							visited[map[nx][ny]] = true;
+							if(ny == 0|| nx == N-1 || ny == N-1 || k == size2-1) {
+								dir++;
+								break;
+							}
+						}
+						if(nx == N-1) continue out;
+						for(int k = 0 ; k < size; k++) {
+							nx = nx + dx[dir];
+							ny = ny + dy[dir];
+							if(nx == N || ny == N) continue out;
+							if(visited[map[nx][ny]]) continue out; 
+							visited[map[nx][ny]] = true;
+							if(nx == N-1 || k == size-1) {
+								dir++;
+								break;
+							}
+						}
+						
+						for(int k = 0 ; k < size2; k++) {
+							nx = nx + dx[dir];
+							ny = ny + dy[dir];
+							if(nx == 0 || ny == N) continue out;
+							if(visited[map[nx][ny]]) continue out; 
+							visited[map[nx][ny]] = true;
+							if(ny == N-1 || k == size2-1) {
+								dir++;
+								break;
+							}
+						}
+						if(nx == 0 || ny == 0) continue out;
+						for(int k = 0 ; k < size; k++) {
+							nx = nx + dx[dir];
+							ny = ny + dy[dir];
+							if(nx == i && ny == j ) break;
+							if(nx == 0 || ny == 0) continue out;
+							if(visited[map[nx][ny]]) continue out; 
+							visited[map[nx][ny]] = true;
+							if(nx == 0 || k == size-1) {
+								dir++;
+								break;
+							}
+						}
+						if(nx==i && ny == j) { // 복귀에 성공헀으면,
+							count();
+						}
+					}
+				}
+			}
+			
+		}
+		
+	}
+	private static void count() {
+		int sum = 0;
+		for(int i =0; i < 101; i++) {
+			if(visited[i]) {
+				sum++;
+			}
+		}
+		if (sum < 4) return;
+		answer = Math.max(answer, sum);
+	}
+	
+}
--- Java/SWTest/SW_2115_벌꿀채취.java
+++ Java/SWTest/SW_2115_벌꿀채취.java
@@ -6,7 +6,12 @@ import java.util.StringTokenizer;
 
 public class SW_2115_벌꿀채취 {
 	static int N, M, C; // M은 선택한 벌통의 사이즈야(크기)
-	static int[][] map;
+	static int[][] map, honey;
+	static int max;
+	static boolean[][] visited;
+	static int incomes[];
+	static boolean[] selected;
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
@@ -18,6 +23,9 @@ public class SW_2115_벌꿀채취 {
 			M = Integer.parseInt(st.nextToken());
 			C = Integer.parseInt(st.nextToken());
 			map = new int[N][N];
+			visited =new boolean[N][N];
+			selected = new boolean[M];
+			honey = new int[2][M];
 			
 			for(int i =0 ; i < N ; i++) {
 				st = new StringTokenizer(br.readLine());
@@ -25,9 +33,64 @@ public class SW_2115_벌꿀채취 {
 					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			
 			// implement my logic here
+			max = 0;
+			// 조합으로 0~n^2-1까지 숫자 중 m개씩 2개 골라, 중복되면 안돼
+			combination(visited, 0,0);
+			sb.append("#").append(test_case).append(" ").append(max).append("\n");
+		}
+		System.out.print(sb);
+	}
+	private static void combination(boolean[][] visited, int start, int cnt) {
+		if(cnt == 2) { //2명 골랐으면
+			incomes = new int[2];
+			getMaxIncome(0,0,0); // 0번 일꾼의 maxIncome
+			getMaxIncome(1,0,0); // 1번 일꾼의 maxIncome
+			max = Math.max(max, incomes[0]+incomes[1]); // 기존 값이랑 비교
+			return;
+		}
+		for(int i = start; i < N ; i++) {
+			boolean tf = false;
+			for(int j = 0; j<N;j++) {
+				if(j+M > N) continue; // 범위 밖이면 x
+				for(int k =0; k < M ; k++) { // M개 만큼
+					if(visited[i][j+k]) { // 방문된 곳이면
+						tf = true;
+						break;
+					}
+				}
+				if(tf) continue; // continue
+				for(int k = 0; k<M; k++) {
+					visited[i][j+k] = true; // 방문처리
+					honey[cnt][k] = map[i][j+k];
+				}
+				combination(visited, start, cnt+1);
+				
+				for(int k = 0;k <M ; k++) 
+					visited[i][j+k] = false; // 방문해제
+				
+			}
 		}
 	}
+	
+	
+	private static void getMaxIncome(int worker, int sum, int income) {
+		incomes[worker] = Math.max(incomes[worker], income);
+		for(int i =0; i < M ; i++) {
+			if(selected[i]) continue;
+			if( sum + honey[worker][i] <= C) { // 채취할 수 있는거면
+				selected[i] = true;
+				getMaxIncome(worker, sum+honey[worker][i], income+honey[worker][i]*honey[worker][i]);
+				selected[i] = false;
+			}
+		}
+	}
+	
 
+	
+	
+	
+	
+	
+	
 }
