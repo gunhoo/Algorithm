0903b12244283a048d046f343f49a0918858aab2
Gunhoo Park <gunhoo2016@gmail.com>
2023-03-07 23:33:46 +0900

    solved BJ 2407 조합(BigInteger)

---
+++ Java/Silver/BJ_2407_조합.java
@@ -0,0 +1,32 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+/**
+ * nCr = n! / (n-r)!*r!
+ * @author Gunhoo
+ *
+ */
+public class BJ_2407_조합 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		BigInteger n1 = BigInteger.ONE;
+        BigInteger n2 = BigInteger.ONE;
+        
+        for (int i = 0; i < m; i++) {
+            n1 = n1.multiply(new BigInteger(String.valueOf(n - i)));
+            n2 = n2.multiply(new BigInteger(String.valueOf(i + 1)));
+        }
+
+        BigInteger answer = n1.divide(n2);
+
+        System.out.println(answer);
+	}
+
+}
