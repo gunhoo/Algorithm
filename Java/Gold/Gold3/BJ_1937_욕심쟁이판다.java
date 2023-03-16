d3f5201856be5a5fcf75d0d6bcd556f6fb59c85d
gunhoo <gunhoo2016@gmail.com>
2023-03-16 12:21:44 +0900

    update BJ 1937(Dynamic Programming, Memoization)

--- Java/Gold/Gold3/BJ_1937_욕심쟁이판다.java
+++ Java/Gold/Gold3/BJ_1937_욕심쟁이판다.java
@@ -33,35 +33,34 @@ public class BJ_1937_욕심쟁이판다 {
 		for(int i =0 ;i < N ;i++) {
 			for(int j = 0 ; j < N ;j++) {
 //				dfs(i, j, 1);
-				 memo(i, j,1);
+				 memo(i, j,1); // i.j가 가장 큰 곳이라고 생각했을 때 memoization 시작
 			}
 		}
 		print();
 		System.out.println(answer);
 	}
-	static void print() {
+	static void print() { // 최댓값 갱신
 		for(int i =0; i < N ;i++) {
 			for(int j =0; j < N ;j++) {
-				if( answer < dp[i][j]) answer = dp[i][j];
+				if(answer < dp[i][j]) answer = dp[i][j];
 			}
 		}
 	}
 	static int[] dx = {1,-1,0,0};
 	static int[] dy = {0,0,1,-1};
-	private static int memo(int a, int b, int depth) {
-		for(int i = 0; i< 4; i++) {
+	private static int memo(int a, int b, int depth) { // 메모이제이션(큰 곳부터 작은곳으로가)
+		for(int i = 0; i< 4; i++) { // 4방향으로
 			int nx = a + dx[i];
 			int ny = b + dy[i];
-			if(0<=nx && nx< N && 0<=ny && ny <N && map[nx][ny] < map[a][b]) {
-				if(dp[nx][ny] !=1 ) {
-					dp[a][b] = Math.max(dp[nx][ny]+1, dp[a][b]);
-				}else { 
-					dp[a][b] = Math.max(memo(nx, ny, depth+1)+1, dp[a][b]);
+			if(0<=nx && nx< N && 0<=ny && ny <N && map[nx][ny] < map[a][b]) { // 이동한 곳이 더작으면,
+				if(dp[nx][ny] !=1 ) { // 그곳이 방문한 곳이라면, 
+					dp[a][b] = Math.max(dp[nx][ny]+1, dp[a][b]); // 그곳에서거리+1과 현재 내가 가진 값이랑 비교 
+				}else {  // 이동한 곳이 방문하지 않은 곳이라면,
+					dp[a][b] = Math.max(memo(nx, ny, depth+1)+1, dp[a][b]); // 이동한 곳에서+1증가한 거리와 현재 내 값이랑 비교 
 				}
-				
 			}
 		}
-		return dp[a][b];
+		return dp[a][b]; // 그곳의 최대 거리값 리턴
 	}
 	private static void dfs(int a, int b, int depth) {
 		for(int i =0; i < 4; i++) {
