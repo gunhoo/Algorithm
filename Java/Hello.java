46b28c0f626503eeefd1c7a6f1f6cf8d84417109
Gunhoo Park <gunhoo2016@gmail.com>
2023-03-19 15:44:44 +0900

    update test file

--- Java/Hello.java
+++ Java/Hello.java
@@ -1,32 +1,16 @@
 import java.util.Scanner;
 
 public class Hello {
-
-	
-	public static void main2(String[] args) {
-		Scanner sc =new Scanner(System.in);
-		long n = sc.nextLong();
-		long[] dp = {0,1};
-		long sum = 1;
-		if(n %2 == 0 ) {
-			
-		}
-	}
 	
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
-		long n = sc.nextLong();
-		long[] dp = {0,1};
-		long sum = 1;
-		for(long i = 1; i < n ; i++) {
-			long tmp = dp[0];
-			dp[0] = dp[1];
-			dp[1] = (tmp+dp[1]);
-			sum = sum+(dp[1]*dp[1]);
-			System.out.println(sum);
-		}
-		sum %= 1000000007;
-		System.out.println(sum);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		System.out.println((a+b)%c);
+		System.out.println(((a%c)+(b%c))%c);
+		System.out.println((a*b)%c);
+		System.out.println(((a%c)*(b%c))%c);
 	}
 
 }
