7c610743e2d8fc4a4c9506aa370410fa96c30447
gunhoo <gunhoo2016@gmail.com>
2023-03-23 17:58:10 +0900

    solving...

---
+++ Java/Platinum/BJ_1786_찾기.java
@@ -0,0 +1,14 @@
+package Platinum;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_1786_찾기 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		char[] input = br.readLine().toCharArray();
+		
+	}
+
+}
