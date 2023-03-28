b7009def2003b31d2dcfb28f6359d6aa76fbc27a
gunhoo <gunhoo2016@gmail.com>
2023-03-28 17:49:46 +0900

    solved BJ 1149 DP

--- Java/Silver/BJ_1149_RGB거리.java
+++ Java/Silver/BJ_1149_RGB거리.java
@@ -11,19 +11,19 @@ public class BJ_1149_RGB거리 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
-		int[][] dp = new int[N+1][3];
-		for(int i =1; i <= N ;i++) {
+		int[][] dp = new int[N+1][3]; // DP 배열 생성
+		for(int i =1; i <= N ;i++) { 
 			st = new StringTokenizer(br.readLine());
-			Arrays.fill(dp[i], Integer.MAX_VALUE);
+			Arrays.fill(dp[i], Integer.MAX_VALUE); // 최솟값을 구하기위해 기본값을 최댓값으로 설정
 			for(int j =0; j < 3; j++) {
-				int color = Integer.parseInt(st.nextToken());
-				for(int k = 0; k < 3; k++) {
-					if(k == j ) continue;
-					dp[i][j] = Math.min(dp[i][j], color+dp[i-1][k]);
+				int color = Integer.parseInt(st.nextToken()); // 값 입력받고
+				for(int k = 0; k < 3; k++) { // 윗칸을 본다
+					if(k == j ) continue; // 같은 칸이면 패스
+					dp[i][j] = Math.min(dp[i][j], color+dp[i-1][k]); // 현재 자신의 값과 자신의윗줄의값+현재color의 값을 넣어 memoization을 갱신한다
 				}
 			}
 		}
-		System.out.println(Math.min(Math.min(dp[N][0], dp[N][1]), dp[N][2]));
+		System.out.println(Math.min(Math.min(dp[N][0], dp[N][1]), dp[N][2])); // 3개중 최솟값 출력
 	}
 
 }
