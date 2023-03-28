6e7489d24e5fd5214c3614ac994c0905417d278f
Gunhoo Park <gunhoo2016@gmail.com>
2023-03-29 00:52:33 +0900

    update BJ 20057(simulation, implementation) with annotation

--- Java/Gold/Gold3/BJ_20057_마법사상어와토네이도.java
+++ Java/Gold/Gold3/BJ_20057_마법사상어와토네이도.java
@@ -61,11 +61,11 @@ public class BJ_20057_마법사상어와토네이도 {
 				for(int i =0 ;i < 10; i++) {
 					int nx = x+direction[i][0];
 					int ny = y+direction[i][1];
-					if (i < 9) movingDust[i] = dust * ratio[i] / 100;
-					else {
-						movingDust[9] = dust;
+					if (i < 9) movingDust[i] = dust * ratio[i] / 100; // 비율만큼,
+					else { // 10번째는(남아있는 a 양)
+						movingDust[9] = dust; // 총먼지양에서
 						for(int tmp = 0; tmp < 9 ; tmp++) {
-							movingDust[9] -= movingDust[tmp];
+							movingDust[9] -= movingDust[tmp]; // 흩어진 먼지양 빼주면 돼
 						}
 					}
 					if(movingDust[i] == 0) continue;
@@ -78,24 +78,23 @@ public class BJ_20057_마법사상어와토네이도 {
 				map[x][y] = 0; // 토네이도 끝났으면 그자리는 0
 			}			
 			// x좌표, y좌표 이동해야해
-			// 이동하는거만들어야하고, 몇칸이동하는지도 만들어아해
 			sizeCond++;
 			if(sizeCond == size) { // 크기만큼 돌았으면
 				sizeCond =0; // 컨디션 조건 초기화
 				turn++; // 턴 조건추가
 				dir = (dir+1)%4; // 방향전환
 			}
-			if(turn == 2) {
-				turn =0;
-				size++;
+			if(turn == 2) { // 두번회전했으면
+				turn =0; // 초기화
+				size++; // 변의 길이 추가
 			}
-			if(dir == 1) {
+			if(dir == 1) { // 왼쪽으로
 				y-=1;
-			}else if(dir == 2) {
+			}else if(dir == 2) { // 아래쪽으로
 				x +=1;
-			}else if(dir == 3) {
+			}else if(dir == 3) { // 오른쪽으로
 				y += 1;
-			}else {
+			}else { // 위쪽으로
 				x -=1;
 			}
 		}		
@@ -111,5 +110,4 @@ public class BJ_20057_마법사상어와토네이도 {
 		}
 		return direction4;
 	}
-
 }
