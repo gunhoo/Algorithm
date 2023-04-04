071775ea798355373efb48c2ad3b38cc9228f08e
gunhoo <gunhoo2016@gmail.com>
2023-04-04 16:37:03 +0900

    solved BJ 1786 (KMP)

--- Java/Platinum/BJ_1786_찾기.java
+++ Java/Platinum/BJ_1786_찾기.java
@@ -2,13 +2,48 @@ package Platinum;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class BJ_1786_찾기 {
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-		char[] input = br.readLine().toCharArray();
+		char[] text = br.readLine().toCharArray();
+		char[] pattern = br.readLine().toCharArray();
+		StringBuilder sb = new StringBuilder();
+		int tLength = text.length, pLength = pattern.length;
 		
+		// 부분일치테이블 만들기 : KMP의 아이디어를 똑같이 적용, W에서 W를 찾는 듯한 행위를 해서...
+		int[] pi = new int[pLength];
+		
+		// i:접미사 포인터(i=1부터 시작: 우리는 부분일치테이블를 만드는게 목적이므로 첫글자 틀리면 0위치로 가야하므로.), j:접두사 포인터
+	    for(int i=1, j=0; i<pLength; i++){
+	        while(j > 0 && pattern[i] != pattern[j]) j = pi[j-1]; 
+	        
+	        if(pattern[i] == pattern[j]) pi[i] = ++j;
+	        else pi[i] = 0;
+	    }
+	    
+	    int cnt = 0;
+		// i : 텍스트 포인터 , j: 패턴 포인터 
+		for(int i=0,j=0; i<tLength; ++i) { 
+			while(j>0 && text[i] != pattern[j]) j = pi[j-1]; 
+			if(text[i] == pattern[j]) { //두 글자 일치
+				if(j == pLength-1) { // j가 패턴의 마지막 인덱스라면 
+					cnt++; // 카운트 증가
+					sb.append(i-j+1).append(" ");  
+					j=pi[j]; 
+				}else { 
+					j++;
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+		if(cnt>0) {
+			System.out.println(sb);;
+		}		
 	}
 
 }
