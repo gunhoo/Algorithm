63445549502c6cd06b59b70c0b273e78fe3b767b
gunhoo <gunhoo2016@gmail.com>
2023-04-19 09:08:05 +0900

    solved BJ 1162 Dijkstra with Dynamic Programming

--- Java/Gold/Gold1/BJ_1162_도로포장.java
+++ Java/Gold/Gold1/BJ_1162_도로포장.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 /**
@@ -14,68 +15,77 @@ import java.util.StringTokenizer;
  *
  */
 public class BJ_1162_도로포장 {
-	private static int N, M, K, distance[];
+	private static int N, M, K;
 	private static final int INF=Integer.MAX_VALUE;
 	private static ArrayList<Node> graph[];
+	static long[][] dp;
 	
-	static class Node implements Comparable<Node>{
-		int node, weight;
-		public Node(int n, int w) {
+	static class Node{
+		int node, count;
+		long weight;
+		
+		public Node(int n, long w, int c) {
 			this.node = n;
 			this.weight = w;
-			
-		}
-		@Override
-		public int compareTo(Node o) {
-			return this.weight-o.weight;
+			this.count = c;
 		}
 	}
 	public static void main(String[] args) throws Exception {
 		init();
-		dijkstra(0);
+		dijkstra();
+		long min = Long.MAX_VALUE;
+        for (long a : dp[N]) {
+            min = Math.min(a, min);
+        }
+        System.out.println(min);
 	}
 	
 	private static void init() throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		graph = new ArrayList[N+1];
-		visited = new boolean[N+1];
-		distance = new int[N+1];
-		Arrays.fill(distance, INF);
-		distance[1] = 0;
-		
-		for(int i =0 ; i < N+1; i++) graph[i] = new ArrayList<>();
-		
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
+		
+		graph = new ArrayList[N+1];
+		visited = new boolean[N+1];
+		dp = new long[N+1][K+1];
+
+		for(int i =0 ; i < N+1; i++) {
+			graph[i] = new ArrayList<>();
+			Arrays.fill(dp[i], Long.MAX_VALUE);
+		}
 		
 		for(int i =0 ;i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			graph[a].add(new Node(b, c));
-			graph[b].add(new Node(a, c));
+			graph[a].add(new Node(b, c, 0));
+			graph[b].add(new Node(a, c, 0));
 		}
 	}
 	
 	private static boolean[] visited; 
-	private static void dijkstra(int cnt ) {
-		if(cnt == K) {
-			
-			return;
-		}
+	private static void dijkstra() {
+		PriorityQueue<Node> pq = new PriorityQueue<Node>(Comparator.comparingLong(o -> o.weight));
+		pq.offer(new Node(1, 0, 0));
+		dp[1][0] = 0;
 		
-		PriorityQueue<Node> pq = new PriorityQueue<Node>();
-		pq.offer(new Node(1, 0));
 		while(!pq.isEmpty()) {
-			int curNode = pq.poll().node;
+			Node curNode = pq.poll();
+			if(curNode.weight > dp[curNode.node][curNode.count]) continue;
 			
-			for(Node next : graph[curNode]) {
-				if(distance[next.node] > distance[curNode] + next.weight) {
-					distance[next.node] = distance[curNode] + next.weight;
-					pq.offer(new Node(next.node, distance[next.node]));
+			for(Node next : graph[curNode.node]) {
+				long nextDistance = next.weight + curNode.weight;
+				if(nextDistance < dp[next.node][curNode.count]) {
+					dp[next.node][curNode.count] = nextDistance;
+					pq.offer(new Node(next.node, nextDistance, curNode.count));
+				}
+				
+				if(curNode.count < K && curNode.weight < dp[next.node][curNode.count+1]) {
+					dp[next.node][curNode.count+1] = curNode.weight;
+					pq.offer(new Node(next.node, curNode.weight, curNode.count+1));
 				}
 			}
 			
