67c93b32e6b4d3a97eb297bc1564acf446d554be
Gunhoo Park <gunhoo2016@gmail.com>
2023-04-25 23:31:55 +0900

    solved BJ 10217 KCM Travel(Dijkstra with Dynamic Programming)

--- Java/Platinum/BJ_10217_KCMTravel.java
+++ Java/Platinum/BJ_10217_KCMTravel.java
@@ -3,6 +3,7 @@ package Platinum;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -15,7 +16,7 @@ import java.util.StringTokenizer;
  */
 public class BJ_10217_KCMTravel {
 	private static int N, M, K;
-	static class Node implements Comparator<Node>{
+	static class Node implements Comparable<Node>{
 		int to, cost, distance;
 
 		public Node(int to, int cost, int distance) {
@@ -26,13 +27,18 @@ public class BJ_10217_KCMTravel {
 		}
 
 		@Override
-		public int compare(Node o1, Node o2) {
-			return o1.distance-o2.distance;
+		public int compareTo(Node o) {
+			if(this.distance < o.distance) return -1;
+			else if(this.distance == o.distance) {
+				if(this.cost < o.cost) return -1;
+				return 0;
+			}
+			return 1;
 		}
 		
 	}
 	private static ArrayList<Node> graph[];
-	private static int[][] dp, cost;
+	private static int[][] cost;
 	
 	
 	public static void main(String[] args) throws Exception{
@@ -45,15 +51,11 @@ public class BJ_10217_KCMTravel {
 			M = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
 			
-			dp = new int[N+1][N+1];
-			cost = new int[N+1][N+1];
+			cost = new int[N+1][M+1];
 			graph = new ArrayList[N+1];
 			for(int i = 0; i < N+1; i++) {
 				graph[i] = new ArrayList<>();
-				for(int j =0 ; j < N+1; j++) {
-					dp[i][j] = Integer.MAX_VALUE;
-					cost[i][j] = 0;
-				}
+				Arrays.fill(cost[i], Integer.MAX_VALUE);
 			}
 			
 			for(int i = 0; i < K ;i++) {
@@ -66,18 +68,31 @@ public class BJ_10217_KCMTravel {
 			}
 			
 			dijkstra();
-			System.out.println(); // dp[?][N]==Integer.MAX_VALUE?"Poor KCM":dp[?][N]
+			int min = Integer.MAX_VALUE;
+			for(int i = 0; i <= M; i++) min = Math.min(min, cost[N][i]);
+			System.out.println(min==Integer.MAX_VALUE?"Poor KCM":min); // 
 		}
 	}
 	
 	private static void dijkstra() {
 		PriorityQueue<Node> pq = new PriorityQueue<>();
 		pq.offer(new Node(1, 0, 0));
+		cost[1][0] = 0;
 		while(!pq.isEmpty()) {
 			Node curNode = pq.poll();
-			
+			if(curNode.to == N) break;
 			for(Node next : graph[curNode.to]) {
-				
+				if(curNode.cost + next.cost > M) continue;
+				if(cost[next.to][curNode.cost + next.cost] <= next.distance+curNode.distance) {
+					continue;
+				}
+				for(int j = curNode.cost + next.cost; j<= M ; j++) {
+					if(cost[next.to][j] > next.distance+curNode.distance) {
+						cost[next.to][j] = next.distance+curNode.distance;
+					}
+				}
+				cost[curNode.to][curNode.cost + next.cost] = next.distance+curNode.distance;
+				pq.offer(new Node(next.to, curNode.cost + next.cost, next.distance+curNode.distance));
 			}
 		}
 	}
